c2e953fba4604063e48c3525ebb84a9e326ee454
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-11-07 20:11:16 +0100

    Add all services so far to the container

--- src/container/container.ts
+++ src/container/container.ts
@@ -1,8 +1,12 @@
 import { Container } from 'inversify';
 import ProductRepository from '../entities/repositories/ProductRepository';
+import ProductValidator from '../services/validation/ProductValidator';
+import ProductController from '../controllers/ProductController';
 
 const container = new Container();
 
 container.bind<ProductRepository>(ProductRepository).to(ProductRepository);
+container.bind<ProductValidator>(ProductValidator).to(ProductValidator);
+container.bind<ProductController>(ProductController).to(ProductController);
 
 export default container;
