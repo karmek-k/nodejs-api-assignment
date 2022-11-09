91dfc8ce5ea6ce48bcb018c6394a805cfdf4c8e4
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-11-09 19:45:36 +0100

    Implement ProductController.ts and remove unnecessary code

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,48 +1,74 @@
-import { Request, Response } from 'express';
-import BaseResourceController from './BaseResourceController';
 import { inject } from 'inversify';
 import ProductRepository from '../entities/repositories/ProductRepository';
 import { symbols } from '../container/symbols';
 import {
+  BaseHttpController,
   controller,
   httpDelete,
   httpGet,
   httpPost,
   httpPut,
-  request,
-  response
+  requestBody,
+  requestParam
 } from 'inversify-express-utils';
+import Product, {
+  ProductCreateDto,
+  ProductUpdateDto
+} from '../entities/Product';
+import ProductValidator from '../services/validation/ProductValidator';
+import { JsonResult } from 'inversify-express-utils/lib/results';
 
 @controller('/products')
-export default class ProductController extends BaseResourceController {
+export default class ProductController extends BaseHttpController {
   constructor(
-    @inject(symbols.ProductRepository) private repo: ProductRepository
+    @inject(symbols.ProductRepository) private repo: ProductRepository,
+    @inject(symbols.ProductValidator) private validator: ProductValidator
   ) {
     super();
   }
 
-  @httpPost('/')
-  async create(
-    @request() req: Request,
-    @response() res: Response
-  ): Promise<void> {
-    res.json(await this.repo.create(req.body));
-  }
-
-  @httpDelete('/:id')
-  delete(@request() req: Request, @response() res: Response): void {}
-
   @httpGet('/')
-  async getAll(
-    @request() req: Request,
-    @response() res: Response
-  ): Promise<void> {
-    res.json(await this.repo.list());
+  async getAll(): Promise<Product[]> {
+    return await this.repo.list();
   }
 
   @httpGet('/:id')
-  getOne(@request() req: Request, @response() res: Response): void {}
+  async getOne(@requestParam('id') id: number): Promise<Product | JsonResult> {
+    const item = await this.repo.details(id);
 
-  @httpPut('/:id')
-  update(@request() req: Request, @response() res: Response): void {}
+    if (item) {
+      return item;
+    }
+
+    return this.json({ msg: 'Not found' }, 404);
+  }
+
+  @httpPost('/')
+  async create(@requestBody() dto: ProductCreateDto): Promise<Product> {
+    return this.repo.create(dto);
+  }
+
+  @httpPut('/')
+  async update(
+    @requestBody() dto: ProductUpdateDto
+  ): Promise<Product | JsonResult> {
+    const item = await this.repo.update(dto);
+
+    if (item) {
+      return item;
+    }
+
+    return this.json({ msg: 'Not found' }, 404);
+  }
+
+  @httpDelete('/:id')
+  async delete(@requestParam('id') id: number): Promise<Product | JsonResult> {
+    const item = await this.repo.delete(id);
+
+    if (item) {
+      return item;
+    }
+
+    return this.json({ msg: 'Not found' }, 404);
+  }
 }
